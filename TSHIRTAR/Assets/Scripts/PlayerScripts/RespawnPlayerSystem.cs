719c0f9c7672c01d1ed42b49e82741fad7c2a2ba
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-11 11:13:22 -0600

    Update RespawnPlayerSystem.cs

--- TSHIRTAR/Assets/Scripts/PlayerScripts/RespawnPlayerSystem.cs
+++ TSHIRTAR/Assets/Scripts/PlayerScripts/RespawnPlayerSystem.cs
@@ -6,25 +6,28 @@ using TMPro;
 public class RespawnPlayerSystem : MonoBehaviour
 {
     [Header("Player")]
-    public GameObject player;
+    public GameObject player; //Player game object
 
-    [Header("Score Text")]
-    public TextMeshPro livesText;
+    [Header("Lives Text")]
+    public TextMeshPro livesText; //Text for the lives
 
     [Header("Variables")]
-    public bool isDead = false;
-    public float restartTimer = .5f;
-    public int livesLeft = 3;
+    public bool isDead = false; //Boolean to check if the Player is dead
+    public float restartTimer = .5f; //Timer for when the game can start again on the current level
+    public int livesLeft = 3; //Amount of lives left
 
     [Header("GameOverScript")]
-    public GameOverScript gos;
+    public GameOverScript gos; //GameOverScript script
 
     // Update is called once per frame
     void Update()
     {
-        
+        //Checks to see if the isDead is true and there still lives left
         if (isDead == true && livesLeft > 0)
-        {
+        {   
+            //If yes, we will update the Lives text to the current amount of lives left and have the restart timer counting down
+            //When the timer is less than 0, we will instantiate a new Player ship, set its parent to the Image Target
+            //And set the timer back to 1 second and isDead to false
             livesText.text = "LIVES:" + livesLeft.ToString();
             restartTimer -= Time.deltaTime;
             if(restartTimer < 0)
@@ -35,7 +38,8 @@ public class RespawnPlayerSystem : MonoBehaviour
                 isDead = false;
             }
         }
-
+        
+        //If there are no lives left, then we will set isGameOver to true, destroy the barrier and enemy game objects and set livesLeft to -1 to make sure the game doesn't create another player game object
         if(livesLeft == 0)
         {
             livesText.text = "LIVES:" + livesLeft.ToString();
