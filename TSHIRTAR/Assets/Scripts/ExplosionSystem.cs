e7a2e295b243b31ed2dba404de4bc2e34e11dd03
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-10 23:13:14 -0600

    Update ExplosionSystem.cs

--- TSHIRTAR/Assets/Scripts/ExplosionSystem.cs
+++ TSHIRTAR/Assets/Scripts/ExplosionSystem.cs
@@ -7,6 +7,7 @@ public class ExplosionSystem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        //When the explosion object is created, this script will set the object's parent to Image Target and destroy itself after 0.2 seconds
         transform.SetParent(GameObject.FindGameObjectWithTag("MainGame").transform);
         Destroy(gameObject, .2f);
     }
