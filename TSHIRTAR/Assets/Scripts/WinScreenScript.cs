c4763cfce94e0e3a8bbe465a4958d815e2421a2a
JPierson1313 <jpierso3@masonlive.gmu.edu>
2021-02-10 22:59:07 -0600

    Update WinScreenScript.cs

--- TSHIRTAR/Assets/Scripts/WinScreenScript.cs
+++ TSHIRTAR/Assets/Scripts/WinScreenScript.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class WinScreenScript : MonoBehaviour
 {
-    public NextWaveSystem nws;
-    public GameObject winScreenLayout;
-    public GameObject startScreenLayout;
+    public NextWaveSystem nws; //NewWaveSystem Script
+    public GameObject winScreenLayout; //winScreenLayout Canvas
+    public GameObject startScreenLayout; //startScreenLayout Canvas
 
+    //When the player taps on the button to play the game again, the wave counter will reset to 1, the canStartNextWave boolean will be set to false
+    //The WinScreen Canvas would be inactive and the StartScreen Canvas would become active to start the game over again
     public void PlayAgainButton()
     {
         nws.waveCounter = 1;
